03b9830026f4ad6aa1f223125b87cb85d3c162d1
Maryan Shapkaroski <maryan.shapkaroski@gmail.com>
2021-03-15 21:39:20 +0100

    Implemented SQL

--- MyBlog/Startup.cs
+++ MyBlog/Startup.cs
@@ -29,7 +29,8 @@ namespace MyBlog
         {
             services.AddControllersWithViews();
             services.AddTransient<IBlogService, BlogService>();
-            services.AddTransient<IBlogRepository, BlogFileRepository>();
+            //services.AddTransient<IBlogRepository, BlogFileRepository>();
+            services.AddTransient<IBlogRepository, BlogSqlRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
